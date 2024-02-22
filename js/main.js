b7c52fa253e61568f944bd94f748b2ffa8b14b68
Mikhail <agent210@site69.ru>
2024-02-22 18:19:28 +0300

    v1.2.9

--- js/main.js
+++ js/main.js
@@ -2,5 +2,8 @@ $(document).ready(function() {
     $('.possibilities__list-item').click(function() {
         $('.possibilities__list-item').removeClass('possibilities__list-item--active');
         $(this).addClass('possibilities__list-item--active');
+        var dataNumber = $(this).attr('data-number');
+        $('.possibilities__image').removeClass('possibilities__image--active');
+        $('.possibilities__image[data-number=' + dataNumber + ']').addClass('possibilities__image--active')
     });
 });
