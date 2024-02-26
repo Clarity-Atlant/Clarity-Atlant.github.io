7fb66b8332e53d3deb48931bbb40245c5eb844d2
Mikhail <agent210@site69.ru>
2024-02-26 15:19:06 +0300

    v1.3.2
    В первом приближении сделана мобильная версия.

--- js/main.js
+++ js/main.js
@@ -6,4 +6,26 @@ $(document).ready(function() {
         $('.possibilities__image').removeClass('possibilities__image--active');
         $('.possibilities__image[data-number=' + dataNumber + ']').addClass('possibilities__image--active')
     });
+    $('.main-description__technologies-description-list-item-toggle').click(function() {
+        if($(this).find('img').attr('src') == 'img/minus.png') {
+            $(this).parent().siblings('.main-description__technologies-description-list-item-list').css('display', 'block');
+            $(this).parent().siblings('.main-description__technologies-description-list-item-list').slideToggle(150);
+            $(this).find('img').attr('src', 'img/plus.png');
+            $(this).css('top', '30px');
+        } else {
+            $(this).parent().siblings('.main-description__technologies-description-list-item-list').css('display', 'none');
+            $(this).parent().siblings('.main-description__technologies-description-list-item-list').slideToggle(150);
+            $(this).find('img').attr('src', 'img/minus.png');
+            $(this).css('top', '25px');
+        }
+    });
+    $('.header__mobile-menu').click(function() {
+        if($(this).find('img').attr('src') == 'img/mobile-menu-burger.png') {
+            $(this).find('img').attr('src', 'img/mobile-menu-close.png');
+            $('.mobile-menu-overlay').addClass('mobile-menu-overlay--active')
+        } else {
+            $(this).find('img').attr('src', 'img/mobile-menu-burger.png');
+            $('.mobile-menu-overlay').removeClass('mobile-menu-overlay--active')
+        }
+    });
 });
